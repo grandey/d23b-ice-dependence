10aed430bd3ba7a0872e683edfd21622ead1dced
grandey_t1 <grandey@users.noreply.github.com>
2025-05-27 16:27:59 +0800

    d23b.py: - fig_dependence_table(), fig_total_vs_time(), WORKFLOW_LABELS, WORKFLOW_NOTES, WORKFLOW_COLORS: replace P21+L23 with S20+P21+L23. - sample_trivariate_copula(): use 4 threads.

--- d23b.py
+++ d23b.py
@@ -86,7 +86,7 @@ WORKFLOW_LABELS = {'wf_1e': 'Workflow 1e corr.',  # labels of "workflows" used f
                    'wf_4': 'Workflow 4 corr.',
                    'wf_2e': 'Workflow 2e corr.',
                    'wf_3e': 'Workflow 3e corr.',
-                   'P21+L23': 'P21+L23 ensemble corr.',
+                   'S20+P21+L23': 'S20+P21+L23 ensemble corr.',
                    '0': 'Independence',  # idealized indepedence
                    '1': 'Perfect correlation',  # idealized perfect dependence
                    '10': 'Antarctic correlation',  # perfect dependence & independence
@@ -96,7 +96,8 @@ WORKFLOW_NOTES = {'wf_1e': '$\\bf{Workflow\ 1e}$\n(shared dependence on GSAT;\nE
                   'wf_4': '$\\bf{Workflow\ 4}$\n(structured expert judgment;\nBamber et al., 2019)',
                   'wf_2e': '$\\bf{Workflow\ 2e}$\n(Antarctic model ensemble;\nLevermann et al., 2020)',
                   'wf_3e': '$\\bf{Workflow\ 3e}$\n(Antarctic model ensemble;\nDeConto et al., 2021)',
-                  'P21+L23': '$\\bf{P21\!+\!L23}$\n(Antarctic model ensemble;\nPayne et al., 2021; Li et al., 2023)',
+                  'S20+P21+L23': ('$\\bf{S20\!+\!P21\!+\!L23}$\n(Antarctic model ensemble;\n'
+                                  'Seroussi et al., 2020;\nPayne et al., 2021; Li et al., 2023)'),
                   '0': '$\\bf{Independence}$\n(idealized)',
                   '1': '$\\bf{Perfect\ correlation}$\n(idealized)',
                   '10': '$\\bf{Antarctic\ correlation}$\n(idealized)',
@@ -105,7 +106,7 @@ WORKFLOW_COLORS = {'wf_1e': 'darkblue',  # colors used by ax_total_vs_time(), ax
                    'wf_4': 'darkgreen',
                    'wf_2e': 'lightred',
                    'wf_3e': 'darkred',
-                   'P21+L23': 'purple',
+                   'S20+P21+L23': 'purple',
                    '0': 'lightslategrey',
                    '1': 'brown',
                    '10': 'darkorange',
@@ -730,7 +731,7 @@ def sample_trivariate_copula(cop_workflow='wf_1e', n_samples=1000000, plot=False
     tricop = quantify_trivariate_dependence(cop_workflow=cop_workflow)
     # Simulate data
     print(f'sample_trivariate_copula({cop_workflow}, {n_samples}, {plot}): drawing {n_samples} samples')
-    u_n3 = tricop.simulate(n=n_samples, seeds=[1, 2, 3, 4, 5])
+    u_n3 = tricop.simulate(n=n_samples, seeds=[1, 2, 3, 4, 5], num_threads=4)
     # Plot?
     if plot:
         sns.pairplot(pd.DataFrame(u_n3, columns=[f'u{n+1}' for n in range(3)]), kind='hist')
@@ -1032,7 +1033,7 @@ def fig_illustrate_copula():
     return fig
 
 
-def fig_dependence_table(cop_workflows=('0', '1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'P21+L23'),
+def fig_dependence_table(cop_workflows=('0', '1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'S20+P21+L23'),
                          all_pairs=True, print_tricop=True):
     """
     Plot heatmap table of bivariate copulas for AR6 workflows and ISM ensemble.
@@ -1041,7 +1042,7 @@ def fig_dependence_table(cop_workflows=('0', '1', '10', 'wf_1e', 'wf_4', 'wf_3e'
     ----------
     cop_workflows : tuple of str
         AR6 workflows (e.g. 'wf_1e'), ice sheet model ensemble (e.g. 'P21+L23'), and/or idealized dependence (e.g. '1').
-        Default is ('0', '1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'P21+L23').
+        Default is ('0', '1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'S20+P21+L23').
     all_pairs : bool
         If True (default), include all pairs of dependencies.
     print_tricop : bool
@@ -1341,7 +1342,7 @@ def ax_total_vs_time(cop_workflows=('wf_3e', '0'),
     return ax
 
 
-def fig_total_vs_time(cop_workflows=('1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'P21+L23'),
+def fig_total_vs_time(cop_workflows=('1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'S20+P21+L23'),
                       ref_workflows=('0', '0', '0', '10', '10', '10'),
                       marg_workflow='fusion_1e', marg_scenario='ssp585', marg_years=np.arange(2020, 2101, 10),
                       thresh_for_timing_diff=None, ylim=(-0.2, 2.0)):
@@ -1353,7 +1354,7 @@ def fig_total_vs_time(cop_workflows=('1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'P21+L
     ----------
     cop_workflows : tuple of str
         AR6 workflows (e.g. 'wf_1e'), ice sheet model ensemble (e.g. 'P21+L23'), and/or idealized dependence (e.g. '1').
-        Note, these will be plotted in separate panels. Default is ('1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'P21+L23').
+        Note, these will be plotted in separate panels. Default is ('1', '10', 'wf_1e', 'wf_4', 'wf_3e', 'S20+P21+L23').
     ref_workflows : tuple of str
         Workflows corresponding to the vine copulas to be used as the reference in each panel.
         Default is ('0', '0', '0', '10', '10', '10').
